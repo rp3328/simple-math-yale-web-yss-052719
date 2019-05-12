aef9b09b58f1766d1595efc399d740c5496da5a3
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-12 08:58:58 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,28 @@
 def addition(num1, num2)
+  sum = num1 + num2
 end
 
 def subtraction(num1, num2)
+  sum1 = num1 - num2
 end
 
 def division(num1, num2)
+  sum2 = num1 / num2
 end
 
 def multiplication(num1, num2)
+  sum3 = num1 * num2
 end
 
 def modulo(num1, num2)
+  sum4 = num1%num2
 end
 
 def square_root(num)
+  sum5 = Math.sqrt(9)
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  
 end
